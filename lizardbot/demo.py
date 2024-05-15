819194ef9ac421799e6b9e4e2f61a5777933434a
Denis <claimov@gmail.com>
2024-05-15 16:54:01 +0300

    Implement main menu

---
+++ lizardbot/demo.py
@@ -0,0 +1,42 @@
+from hammett.core import Application, Button
+from hammett.core.constants import  DEFAULT_STATE, SourcesTypes
+from hammett.core.screen import Screen
+from hammett.core.mixins import StartMixin
+from hammett.core.handlers import register_typing_handler
+
+
+class HelloScreen(StartMixin, Screen):
+    description = "Привет, это бот который собирает расписание, выбери дату."
+
+    async def add_default_keyboard(self,_update,_context):
+        return[[
+            Button(
+                '15.04',
+                GetGroup,
+                source_type=SourcesTypes.GOTO_SOURCE_TYPE,
+            )
+
+        ]]
+
+
+class GetGroup(StartMixin, Screen):
+    description = "Пришлите номер группы!"
+    @register_typing_handler
+    async def get_number(self, update, _context):
+        nmbr = update.message.text
+
+
+def main():
+    name = 'Start_Screen'
+    app = Application(
+        name,
+        entry_point=HelloScreen,
+        states={
+            DEFAULT_STATE: [HelloScreen, GetGroup],
+        },
+    )
+    app.run()
+
+
+if __name__ == "__main__":
+    main()
