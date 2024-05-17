8f502088b4ef2b67f5672b07948c40ad3895ac7f
Denis <claimov@gmail.com>
2024-05-17 12:51:17 +0300

    Updated burron handlers

--- lizardbot/demo.py
+++ lizardbot/demo.py
@@ -9,11 +9,13 @@ from hammett.core.handlers import register_button_handler
 from disk import get_filenames
 
 
-
 class HelloScreen(StartMixin, Screen):
     description = "Привет, это бот который собирает расписание, выбери дату."
     files = get_filenames()
 
+    @register_button_handler
+    async  def btn_handler(self, update, context):
+        payload = await self.get_payload()
 
     async def add_default_keyboard(self,update,_context):
         files = get_filenames()
@@ -21,8 +23,9 @@ class HelloScreen(StartMixin, Screen):
         for file in files:
             button = Button(
                 f'{file["name"]}'.replace('.xlsx',''),
-                GetGroup,
+                self.btn_handler,
                 source_type=SourcesTypes.JUMP_SOURCE_TYPE,
+                payload= file["name"]
             )
             file_keyboard.append([button])
             print(file_keyboard[0])
@@ -33,9 +36,21 @@ class HelloScreen(StartMixin, Screen):
 class GetGroup(StartMixin, Screen):
     description = "Пришлите номер группы!"
 
+    async def jump(
+        self: 'Self',
+        update: 'Update',
+        context: 'CallbackContext[BT, UD, CD, BD]',
+        **kwargs: 'Any',
+    ) -> 'State':
+        payload = await self.get_payload(update, context)
+        ...
+
+        return await super().jump(update, s)
+
     @register_typing_handler
     async def get_number(self, update, _context):
         nmbr = update.message.text
+
 
 
 def main():
