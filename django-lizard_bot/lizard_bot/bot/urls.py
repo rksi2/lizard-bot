b10e8f4090a2d8b9b42dea74701aba50e411be30
Denis <claimov@gmail.com>
2024-05-24 13:03:20 +0300

    Deleted Django

--- django-lizard_bot/lizard_bot/bot/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import FileListView, ServiceView, ScheduleTeacherView
-
-urlpatterns = [
-    path("files/", FileListView.as_view(), name="file-list"),
-    path("service/", ServiceView.as_view(), name="service"),
-    path("teachers/", ScheduleTeacherView.as_view(), name="teacher"),
-]
