f5c57b1ee56ef5a495622b79c88c29757ce7678c
dreddxyz <dreddxyz@gmail.com>
2024-05-27 15:53:56 +0300

    PFixed ruff formating

--- lizardbot/screens.py
+++ lizardbot/screens.py
@@ -4,20 +4,24 @@
 Содержит необходимые импорты для корректной работы.
 """
 
+import os
 from typing import Any
 
-import aiohttp
+import httpx
+from dotenv import load_dotenv
 from hammett.core import Button
 from hammett.core.constants import DEFAULT_STATE, SourcesTypes
 from hammett.core.handlers import register_typing_handler
 from hammett.core.mixins import RouteMixin, StartMixin
 from hammett.core.screen import Screen
-from hammett.types import BD, BT, CD, UD, CallbackContext, Keyboard, State, Update
+from hammett.types import BD, BT, CD, UD, CallbackContext, Keyboard, Self, State, Update
 
 from lizardbot import WAITING_FOR_GROUP_NAME, logger
 
-# Настройка логгера
+# Загрузка переменных окружения из .env файла
+load_dotenv()
 
+API_URL = os.getenv('API_URL')
 TIMEOUT = 10
 HTTP_OK = 200
 
@@ -31,32 +35,33 @@ class BaseScreen(Screen):
 class StartScreen(StartMixin, BaseScreen):
     """Начальный экран бота, предоставляет список доступных расписаний."""
 
-    description = "Привет, это бот который собирает расписание, выбери дату."
+    description = 'Привет, это бот который собирает расписание, выбери дату.'
 
     async def add_default_keyboard(
-        self: "StartScreen",
-        _update: Update,
-        _context: CallbackContext[BT, UD, CD, BD],
+        self: 'Self',
+        _update: 'Update',
+        _context: 'CallbackContext[BT, UD, CD, BD]',
     ) -> Keyboard:
         """Добавляет клавиатуру по умолчанию с доступными файлами расписаний."""
-        async with aiohttp.ClientSession() as session, session.get(
-            "http://127.0.0.1:8000/api/files", timeout=TIMEOUT,
-        ) as response:
-            if response.status != HTTP_OK:
-                logger.error(f"Failed to fetch files: {response.status}")
+        async with httpx.AsyncClient() as client:
+            response = await client.get(
+                f'{API_URL}/files/', timeout=TIMEOUT,
+            )
+            if response.status_code != HTTP_OK:
+                logger.error(f'Failed to fetch files: {response.status_code}')
                 return []
-            if response.content_type != "application/json":
-                logger.error(f"Unexpected content type: {response.content_type}")
+            if response.headers['content-type'] != 'application/json':
+                logger.error(f"Unexpected content type: {response.headers['content-type']}")
                 return []
-            files = await response.json()
+            files = response.json()
 
         file_keyboard = []
         for file in files:
             button = Button(
-                f'{file["name"]}'.replace(".xlsx", ""),
+                f'{file["name"]}'.replace('.xlsx', ''),
                 GetGroup,
                 source_type=SourcesTypes.SGOTO_SOURCE_TYPE,
-                payload=file["name"].replace(".xlsx", ""),
+                payload=file['name'].replace('.xlsx', ''),
             )
             file_keyboard.append([button])
 
@@ -66,57 +71,57 @@ class StartScreen(StartMixin, BaseScreen):
 class GetGroup(BaseScreen, RouteMixin):
     """Экран для ввода номера группы пользователем."""
 
-    description = "Пришлите номер группы!"
+    description = 'Пришлите номер группы или фамилию преподавателя!'
     routes = (({DEFAULT_STATE}, WAITING_FOR_GROUP_NAME),)
 
     async def sgoto(
-        self: "GetGroup",
-        update: Update,
-        context: CallbackContext[BT, UD, CD, BD],
+        self: 'Self',
+        update: 'Update',
+        context: 'CallbackContext[BT, UD, CD, BD]',
         **kwargs: Any,
     ) -> State:
         """Переход к экрану ввода номера группы."""
         payload = await self.get_payload(update, context)
-        context.user_data["payload"] = payload
+        context.user_data['payload'] = payload
 
         return await super().sgoto(update, context, **kwargs)
 
     @register_typing_handler
     async def get_schedule(
-        self: "GetGroup", update: Update, context: CallbackContext[BT, UD, CD, BD],
+        self: 'Self', update: 'Update', context: 'CallbackContext[BT, UD, CD, BD]',
     ) -> State:
         """Обрабатывает ввод номера группы и получает расписание."""
-        payload = context.user_data.get("payload")
+        payload = context.user_data.get('payload')
         msg = update.message.text
-        data = {"date": payload, "group": msg}
+        data = {'date': payload, 'group': msg}
 
-        async with aiohttp.ClientSession() as session:
+        async with httpx.AsyncClient() as client:
             if any(char.isdigit() for char in msg):
-                async with session.post(
-                    "http://127.0.0.1:8000/api/service/", json=data, timeout=TIMEOUT,
-                ) as response:
-                    if response.status != HTTP_OK:
-                        logger.error("Ошибка: статус код %d", response.status)
-                    if response.content_type != "application/json":
-                        logger.error(f"Unexpected content type: {response.content_type}")
-                        return await self._get_return_state_from_routes(
-                            update, context, self.routes,
-                        )
-                    schedule = await response.json()
+                response = await client.post(
+                    f'{API_URL}/service/', json=data, timeout=TIMEOUT,
+                )
+                if response.status_code != HTTP_OK:
+                    logger.error('Ошибка: статус код %d', response.status_code)
+                if response.headers['content-type'] != 'application/json':
+                    logger.error(f"Unexpected content type: {response.headers['content-type']}")
+                    return await self._get_return_state_from_routes(
+                        update, context, self.routes,
+                    )
+                schedule = response.json()
                 rasp = GetSchedule()
                 rasp.description = schedule
                 await rasp.jump(update, context)
                 return await self._get_return_state_from_routes(update, context, self.routes)
 
-            async with session.post(
-                "http://127.0.0.1:8000/api/teachers/", json=data, timeout=TIMEOUT,
-            ) as response:
-                if response.status != HTTP_OK:
-                    logger.error("Ошибка: статус код %d", response.status)
-                if response.content_type != "application/json":
-                    logger.error(f"Unexpected content type: {response.content_type}")
-                    return await self._get_return_state_from_routes(update, context, self.routes)
-                schedule = await response.json()
+            response = await client.post(
+                f'{API_URL}/teachers/', json=data, timeout=TIMEOUT,
+            )
+            if response.status_code != HTTP_OK:
+                logger.error('Ошибка: статус код %d', response.status_code)
+            if response.headers['content-type'] != 'application/json':
+                logger.error(f"Unexpected content type: {response.headers['content-type']}")
+                return await self._get_return_state_from_routes(update, context, self.routes)
+            schedule = response.json()
 
         rasp = GetSchedule()
         rasp.description = schedule
@@ -128,17 +133,17 @@ class GetSchedule(BaseScreen):
     """Экран для отображения расписания."""
 
     async def add_default_keyboard(
-        self: "GetSchedule",
-        _update: Update,
-        _context: CallbackContext[BT, UD, CD, BD],
+        self: 'Self',
+        _update: 'Update',
+        _context: 'CallbackContext[BT, UD, CD, BD]',
     ) -> list[list[Button]]:
         """Добавляет клавиатуру с кнопкой возврата к выбору даты."""
         return [
             [
                 Button(
-                    "Вернуться к выбору даты",
+                    'Вернуться к выбору даты',
                     source=StartScreen,
-                    source_type=SourcesTypes.SJUMP_SOURCE_TYPE,
+                    source_type=SourcesTypes.JUMP_SOURCE_TYPE,
                 ),
             ],
         ]
